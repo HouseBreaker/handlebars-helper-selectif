94e9eb500011a47b2dbdf25c34526acf8318220c
Housey <housebreaker1@gmail.com>
2017-03-13 14:19:48 +0200

    fix bug

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = function(lvalue, rvalue, options) {
   if (arguments.length < 3)
     throw new Error("Handlebars SelectIf Helper equal requires 2 parameters")
-  if (lvalue != rvalue) {
+  if (lvalue == rvalue) {
     return 'selected';
   } else {
     return '';
